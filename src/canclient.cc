41cb9f559a89ee7b9c88e536b54baa7806c39239
sleeplessy <sleeplessy@mailbox.org>
2017-12-28 09:21:41 +0800

    First impl

--- src/canclient.cc
+++ src/canclient.cc
@@ -1,12 +1,17 @@
 #include <ccel/canbus.hpp>
 
 #include <algorithm>
+#include <vector>
 #include <boost/asio.hpp>
 #include <iostream>
 #include <rang.hpp>
 #include <string>
-#include <vector>
 int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cout << "USAGE: canclient IP [-p port] [-l local_can_if] [-s]"
+              << std::endl;
+    return -1;
+  }
   std::vector<std::string> __args(argv, argv + argc);
   using namespace boost::asio;
   io_service io;
@@ -26,6 +31,9 @@ int main(int argc, char **argv) {
     auto _silent_option = std::find(__args.begin(), __args.end(), "-s");
     if (_silent_option != __args.end())
       silent = true;
+    auto _color_option = std::find(__args.begin(), __args.end(), "-s");
+    if (_color_option != __args.end())
+      color = true;
     auto _port_option = std::find(__args.begin(), __args.end(), "-p");
 
     if (_port_option != __args.end() && (_port_option + 1 != __args.end())) {
@@ -65,17 +73,27 @@ int main(int argc, char **argv) {
                   << std::setw(8);
         if (!ext)
           std::cout << std::setfill(' ');
-        std::cout << static_cast<unsigned>(__frame.can_id & CAN_EFF_MASK) << " "
-                  << rang::control::forceColor << rang::fg::blue
-                  << (ext ? "EXT" : "STD") << rang::style::reset;
+        std::cout << static_cast<unsigned>(__frame.can_id & CAN_EFF_MASK)
+                  << " ";
+        if (color)
+          std::cout << rang::control::forceColor << rang::fg::blue;
+        std::cout << (ext ? "EXT" : "STD");
+        if (color)
+          std::cout << rang::fg::green;
         std::cout << "[" << static_cast<unsigned>(__frame.can_dlc) << "]"
                   << "\t";
+        if (color)
+          std::cout << rang::fg::yellow;
         for (auto i = 0; i < __frame.can_dlc; i++)
           std::cout << std::setw(2) << std::setfill('0')
                     << static_cast<unsigned>(__frame.data[i]) << " ";
         for (auto i = __frame.can_dlc; i < 8; i++)
           std::cout << "   ";
+        if (color)
+          std::cout << rang::fg::red;
         std::cout << "\tFROM\t" << sock.remote_endpoint() << std::endl;
+        if (color)
+          std::cout << rang::style::reset;
       }
       if (local_transfer)
         local_transfer->write_sock(buff);
